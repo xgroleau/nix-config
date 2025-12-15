2965f8dbbfe214a046da79b509580d89675e7e20
xgroleau <xavgroleau@gmail.com>
2025-12-15 11:56:16 -0500

    nit: git ignore

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My user configuration using home-manager";
+  description = "My NixOS configuration using home-manager";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
